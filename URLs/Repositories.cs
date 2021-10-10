fefe6859ff752b6e341015ba4ddf3d3f76b5aafc
Sam Byass <sam.byass@warwick.ac.uk>
2021-10-10 20:26:20 +0100

    nuke project

--- URLs/Repositories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MelonLoader.URLs
-{
-    internal static class Repositories
-    {
-        internal static string MelonLoader = "https://api.github.com/repos/LavaGang/MelonLoader/releases";
-        internal static string Installer = "https://api.github.com/repos/LavaGang/MelonLoader.Installer/releases";
-    }
-}
